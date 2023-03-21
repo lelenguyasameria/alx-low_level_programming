d41dfbf5060f48e5448e2cf3a0b0b950b76ca157
lelenguyasameria <somearia711@gmail.com>
2023-03-20 22:20:29 -0700

    header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int function1(int arg1, double arg2);
+/*
+ * Description: This function does something.
+ * Parameters:
+ *  - arg1: An integer argument.
+ *  - arg2: A double argument.
+ * Returns:
+ *  - An integer result.
+ */
+
+char function2(char arg);
+/*
+ * Description: This function does something else.
+ * Parameters:
+ *  - arg: A character argument.
+ * Returns:
+ *  - A character result.
+ */
+
+int _putchar(char c);
+/*
+ * Description: This function prints a single character to the console.
+ * Parameters:
+ *  - c: The character to be printed.
+ * Returns:
+ *  - The number of characters printed (always 1).
+ */
+
+#endif /* MAIN_H */
