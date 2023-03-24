bd970b9620baa2c8df0bd6eade71ee9d4c247ce6
lelenguyasameria <somearia711@gmail.com>
2023-03-23 22:44:23 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+char digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+{
+_putchar(digit);
+}
+_putchar('\n');
+}
