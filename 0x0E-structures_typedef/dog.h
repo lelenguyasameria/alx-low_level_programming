1c7b55b727adc0e1d8e8eeb2af2c7826041f5986
lelenguyasameria <somearia711@gmail.com>
2023-04-18 02:26:26 -0700

    main

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize function for struct dog
+ * @d: pointer to dog struct
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ * Return: return pointer to struct
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+return;
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 #ifndef MAIN_H
 #define MAIN_H
 
