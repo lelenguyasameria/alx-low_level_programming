bacb068d8e605df3b928712363ce0929898a3208
lelenguyasameria <somearia711@gmail.com>
2023-04-18 02:15:01 -0700

    main

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
+Prototype: void print_dog(struct dog *d);
+Prototype: dog_t *new_dog(char *name, float age, char *owner);
+Prototype: void free_dog(dog_t *d);
+
+#endif
