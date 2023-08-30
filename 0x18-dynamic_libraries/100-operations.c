6cfaeac8704ecf09ac69e1b956dc586121d331ce
lelenguyasameria <somearia711@gmail.com>
2023-08-29 23:43:51 -0700

    linked python dynamic c library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b == 0) {
+        return 0; // Avoid division by zero
+    }
+    return a / b;
+}
+
+int mod(int a, int b) {
+    if (b == 0) {
+        return 0; // Avoid modulo by zero
+    }
+    return a % b;
+}
+
